035dbb2fe748ca1980c3fc516b5c2f4360c62933
Renato Santana <renato.saoli@gmail.com>
2022-08-22 18:14:46 -0300

    Update cypress.config.js
    padronizando conf

--- src/templates/cypress-with-mocha/cypress.config.js
+++ src/templates/cypress-with-mocha/cypress.config.js
@@ -1,23 +1,25 @@
 const { defineConfig } = require('cypress')
 const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
+async function setupNodeEvents(on, config) {
+  allureWriter(on, config);
+
+  // if version not defined, use local
+  const version = config.env.version || 'qa'
+  // load env from json
+  config.env = require(`./cypress/config/${version}.json`);
+  // change baseUrl
+  config.baseUrl = config.env.baseUrl
+
+  return config;
+}
+
 module.exports = defineConfig({
   viewportWidth: 1024,
   viewportHeight: 800,
   projectId: 'projectId',
   e2e: {
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    setupNodeEvents(on, config) {
-      allureWriter(on, config)
-
-      // if version not defined, use local
-      const version = config.env.version || 'qa'
-      // load env from json
-      config.env = require(`./cypress/config/${version}.json`);
-      // change baseUrl
-      config.baseUrl = config.env.baseUrl
-
-      return config
-    },
+    setupNodeEvents
   },
 })
